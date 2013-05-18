09ffb52afa381a69f4955640d156867cc579580d
Guan Gui <guiguan@gmail.com>
2013-05-19 00:55:01 +1000

    doc update

---
+++ src/Uni Call/Test/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Test
+//
+//  Created by Guan Gui on 16/05/13.
+//  Copyright (c) 2013 Guan Gui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
